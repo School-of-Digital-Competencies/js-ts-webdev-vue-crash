cec6e92e3d67db9855b233be269d86bb82e62829
Aliaksandr Krasnou <aliaksandr.krasnou@ehu.lt>
2024-12-05 13:08:53 +0300

    feat: add example v-for

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+import QuoteCard from './components/QuoteCard.vue';
+
+const quotes = [
+  {
+    author: 'Jason Shetcham',
+    quote: 'Что не убивает, делает сильнее',
+  },
+  {
+    author: 'Alisa Yandex',
+    quote: 'Я подумаю об этом завтра',
+  },
+  {
+    author: 'Пользователь Алисы',
+    quote: 'Алиса, скажи 300',
+  },
+];
 </script>
 
 <template>
   <main class="section columns">
-    <section class="column">Hey Hi</section>
+    <!-- <QuoteCard :item="quotes[0]"></QuoteCard> -->
+
+    <section v-for="quote in quotes" :key="quote.author" class="column">
+      <QuoteCard :item="quote"></QuoteCard>
+    </section>
   </main>
 
   <VueQueryDevtools />
